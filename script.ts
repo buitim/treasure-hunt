551272f7093b63628f961c14c24bbe3c0eca3869
Timothy Bui <29244321+buitim@users.noreply.github.com>
2022-02-05 05:30:22 -0800

    Update script.ts

--- script.ts
+++ script.ts
@@ -14,52 +14,60 @@ const regex: RegExp = /[\d]{6}/gm;
 const query = gql`
   query getEmails($username: String!) {
     emails(username: $username) {
-      token
-      subject
-      sender
-      timestamp
       stripped_text
     }
   }
 `;
 
-const makeStepScreenshot = () => {
-  var i = 0;
-  async function inc(page: any) {
-    return await page.screenshot({ path: config.screenshotsDir + '/' + i++ + '.png' });
+const makeScreenshot = (page: any, username: string) => {
+  let i = 0;
+  async function inc() {
+    return await page.screenshot({
+      path: `${config.screenshotsDir}/${username}/${i++}.png`,
+    });
   }
   return inc;
 };
 
 const run = async () => {
-  if (!fs.existsSync(config.screenshotsDir)) {
-    fs.mkdirSync(config.screenshotsDir);
-  }
-
   console.log('invite:', invite, '\n');
 
+  if (!fs.existsSync(config.screenshotsDir)) fs.mkdirSync(config.screenshotsDir);
   for (let i = 1; i <= config.runs; i++) {
     const username: string = nanoid();
-    const screenshot = makeStepScreenshot();
+
+    const getEmail = async () => {
+      const data: any = await request('https://10minutemail.net/address.api.php?new=1');
+      console.log(data);
+      if (data.mail_get_mail.length == 0) {
+        return "Loi";
+      } else {
+        return data.mail_get_mail;
+      }
+    };
+    const email = await retry(getEmail);
+    if (!fs.existsSync(`${config.screenshotsDir}/${username}`)) fs.mkdirSync(`${config.screenshotsDir}/${username}`);
 
     console.log(`[${i}] Starting run`);
 
-    console.log(`[${i}] Email:`, username + '@emaildrop.io');
+    console.log(`[${i}] Email:`,email);
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    const screenshot = makeScreenshot(page, username);
+
     await page.goto('https://treasure.cloud', { waitUntil: 'load', timeout: 0 });
     await page.goto('https://app.treasure.cloud/auth/signup?code=' + invite, { waitUntil: 'load', timeout: 0 });
     await page.goto('https://app.treasure.cloud/auth/signup?code=' + invite, { waitUntil: 'load', timeout: 0 });
 
-    await screenshot(page);
+    await screenshot();
 
-    await page.type('#mat-input-0', `${username}@emaildrop.io`);
+    await page.type('#mat-input-0', `${email}`);
     await page.type('#mat-input-1', username);
     await page.type('#mat-input-2', config.password);
     await page.type('#mat-input-3', config.password);
 
-    await screenshot(page);
+    await screenshot();
 
     await page.keyboard.press('Enter');
 
@@ -67,22 +75,41 @@ const run = async () => {
 
     await page.waitForNavigation({ waitUntil: 'load', timeout: 0 });
 
-    await page.waitForTimeout(10000);
+    await page.waitForTimeout(8000);
 
-    await screenshot(page);
+    await screenshot();
 
-    const getOTP = async () => {
-      const data: any = await request('https://api.emaildrop.io/graphql', query, { username });
+    // const getOTP = async () => {
+    //   const data: any = await request('https://api.emaildrop.io/graphql', query, { username });
 
-      if (data.emails.length == 0) {
-        return await getOTP();
+    //   if (data.emails.length == 0) {
+    //     return await getOTP();
+    //   } else {
+    //     return regex.exec(data.emails[0].stripped_text.replaceAll(/\s/g, ''))[0];
+    //   }
+    // };
+
+    const getMailId = async () => {
+      const data: any = await request('https://10minutemail.net/address.api.php');
+
+      if (data.mail_list.length < 2) {
+        return await getMailId();
       } else {
-        return regex.exec(data.emails[0].stripped_text.replaceAll(/\s/g, ''))[0];
+        return data.mail_list[0].mail_id;
       }
     };
 
+    const mailId = await retry(getMailId);
+    console.log(`[${i}] Mail Id:`, mailId);
+
+    const getOTP = async () => {
+      const data: any = await request('https://10minutemail.net/readmail.html?mid=' + mailId);
+
+      console.log(data);
+    };
+
     const otp = await retry(getOTP);
-    console.log(`[${i}] Obtained OTP:`, otp);
+    console.log(`[${i}] Otp:`, otp);
 
     const nums = otp.split('');
     await page.type(`#verify-code-form > div:nth-child(1) > div:nth-child(1) > input`, nums[0]);
@@ -92,15 +119,15 @@ const run = async () => {
     await page.type(`#verify-code-form > div:nth-child(2) > div:nth-child(2) > input`, nums[4]);
     await page.type(`#verify-code-form > div:nth-child(2) > div:nth-child(3) > input`, nums[5]);
 
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(1000);
 
-    await screenshot(page);
+    await screenshot();
 
     await page.waitForNavigation({ waitUntil: 'load', timeout: 0 });
 
-    await screenshot(page);
+    await screenshot();
 
-    await page.waitForTimeout(5000);
+    await page.waitForTimeout(2000);
 
     await browser.close();
 
